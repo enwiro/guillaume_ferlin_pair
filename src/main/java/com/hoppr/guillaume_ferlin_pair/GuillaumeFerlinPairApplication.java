d653aa1117370ea9ad60d125c158a644b4bb16b1
tlanord <theo.lanord@ext-leroymerlin.fr>
2023-09-05 17:46:14 +0200

    chore: Add Spring initializer files

---
+++ src/main/java/com/hoppr/guillaume_ferlin_pair/GuillaumeFerlinPairApplication.java
@@ -0,0 +1,13 @@
+package com.hoppr.guillaume_ferlin_pair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuillaumeFerlinPairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuillaumeFerlinPairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoppr/guillaume_ferlin_pair/GuillaumeFerlinPairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoppr.guillaume_ferlin_pair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuillaumeFerlinPairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
